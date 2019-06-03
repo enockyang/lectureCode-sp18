6b69e27fd7276179a010576eed8394ebddbdf3fd
enockyang <enock0504@utexas.edu>
2019-06-03 15:07:39 -0500

    list1_done

--- exercises/lists1/IntList.java
+++ exercises/lists1/IntList.java
@@ -9,17 +9,42 @@ public class IntList {
 
 	/** Return the size of the list using... recursion! */
 	public int size() {
-		return 0;
+		if (this.rest == null){
+			return 1;
+		} else{
+			return 1 + this.rest.size();
+		}
 	}
 
 	/** Return the size of the list using no recursion! */
 	public int iterativeSize() {
-		return 0;
+		IntList p = this;
+		int num = 1;
+		while (p.rest != null){
+			num += 1;
+			p = p.rest;
+		}
+		return num;
 	}
 
 	/** Returns the ith value in this list.*/
 	public int get(int i) {
-		return 0;
+		if ( i == 0){
+			return this.first;
+		} else {
+			return this.rest.get(i-1);
+		}
+	}
+
+	/* return the ith item in the list by interation*/
+	public int iterativeGet(int i){
+		IntList p = this;
+		int index = 0;
+		while (index != i){
+			p = p.rest;
+			index += 1;
+		}
+		return p.first;
 	}
 
 	public static void main(String[] args) {
@@ -27,6 +52,9 @@ public class IntList {
 		L = new IntList(10, L);
 		L = new IntList(5, L);
 
+		System.out.println(L.size());
 		System.out.println(L.iterativeSize());
+		System.out.println(L.get(1));
+		System.out.println(L.iterativeGet(1));
 	}
 } 
--- exercises/lists1/Lists1Exercises.java
+++ exercises/lists1/Lists1Exercises.java
@@ -3,16 +3,28 @@ public class Lists1Exercises {
       * each element incremented by x. L is not allowed
       * to change. */
     public static IntList incrList(IntList L, int x) {
-        /* Your code here. */
-        return L;        
+        IntList p = new IntList(L.first+x, null);
+
+        if (L.rest == null) {
+        	p.rest = null;
+        } else {
+        	p.rest = incrList(L.rest,x);
+        }
+        
+        return p;        
     }
 
     /** Returns an IntList identical to L, but with
       * each element incremented by x. Not allowed to use
       * the 'new' keyword. */
     public static IntList dincrList(IntList L, int x) {
-        /* Your code here. */
-        return L;
+        L.first += x;
+
+        if (L.rest != null) {
+        	L.rest = incrList(L.rest,x);
+        }
+        
+        return L;   
     }
 
     public static void main(String[] args) {
@@ -20,14 +32,16 @@ public class Lists1Exercises {
         L.rest = new IntList(7, null);
         L.rest.rest = new IntList(9, null);
 
-        System.out.println(L.size());
-        System.out.println(L.iterativeSize());
+        //System.out.println(L.size());
+        //System.out.println(L.iterativeSize());
 
         // Test your answers by uncommenting. Or copy and paste the
         // code for incrList and dincrList into IntList.java and
         // run it in the visualizer.
-        // System.out.println(L.get(1));
-        // System.out.println(incrList(L, 3));
-        // System.out.println(dincrList(L, 3));        
+        System.out.println(L.get(1));
+        System.out.println(incrList(L, 3).get(1));
+        System.out.println(L.get(1));
+        System.out.println(dincrList(L, 3).get(1));
+        System.out.println(L.get(1));        
     }
 }
